c097ef06f1ab3b952112f4ed23efc0285673df0d
Mohamed Hossam <m.hossameldine@gmail.com>
2022-03-08 18:54:10 +0200

    Meals

---
+++ src/components/Meals/MealItem/MealItem.js
@@ -0,0 +1,21 @@
+import MealItemForm from './MealItemForm';
+import styles from './MealItem.module.css';
+
+const MealItem = (props) => {
+  const price = `$${props.price.toFixed(2)}`;
+  return (
+    <li className={styles.meal}>
+      <div>
+        <h3> {props.name} </h3>
+        <p className={styles.description}> {props.description} </p>
+        <p className={styles.price}> {price} </p>
+      </div>
+
+      <div>
+        <MealItemForm />
+      </div>
+    </li>
+  );
+};
+
+export default MealItem;
